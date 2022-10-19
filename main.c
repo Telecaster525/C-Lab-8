b67f2c07782f70fc694a14534f541b0de357760f
Said Gadzhiev <94011547+Telecaster525@users.noreply.github.com>
2022-10-19 12:35:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,20 +6,38 @@ int main() {
     gets(string_1);
     char string_2[666];
     gets(string_2);
+
+    
+    printf("#Task 3#\n");
+    printf("Result of string comparison: ");
+    if (strcmp(string_1, string_2) == 0) {
+        printf("Strings are the same");
+    }
+    else {
+        printf("Strings are different");
+    }
+    printf("\n \n");
+
+
     printf("#Task 5#\n");
     strcpy(string_1, string_2);
-    printf("The result of copying the first line to the second: ");
-    printf("%s \n", string_2);
+    printf("The result of copying the first line to the second: %s\n", string_2);
+
+
     printf("#Task 7#\n");
-    printf("Line length \"%s\": %d \n", string_2, strlen(string_2));
+    printf("Line length \"%s\": %lld \n", string_2, strlen(string_2));
+
+
     printf("#Task 9#\n");
     char *pw = strrchr(string_2, 'w');
     if (pw == NULL) {
         printf("Searched file not found\n");
     }
     else {
-        printf("The file you are looking for is at position %d\n", (pw - string_2) + 1);
+        printf("The file you are looking for is at position %lld\n", (pw - string_2)+1);
     }
+
+
     printf("#Task 11#\n");
 
     return 0;
